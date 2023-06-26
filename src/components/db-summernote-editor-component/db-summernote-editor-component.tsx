a88d194065ae214fdae3753ededaa9eed2ec6ade
SaiGovardhana <191fa04292@gmail.com>
2023-06-26 15:30:27 +0530

    Added SummerNote

---
+++ src/components/db-summernote-editor-component/db-summernote-editor-component.tsx
@@ -0,0 +1,23 @@
+import { Component, Host, h, ComponentDidLoad } from '@stencil/core';
+
+@Component({
+  tag: 'db-summernote-editor-component',
+  styleUrl: 'db-summernote-editor-component.css',
+  shadow: true,
+})
+export class DbSummernoteEditorComponent implements ComponentDidLoad{
+
+  componentDidLoad()
+  {
+    
+  }
+  
+  render() {
+    return (
+      <Host>
+        
+      </Host>
+    );
+  }
+
+}
